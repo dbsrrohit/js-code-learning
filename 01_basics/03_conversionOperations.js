e3df8e491950f4cae3df86d2e8910dc4841c72fc
Rohit Prabhakar <54357263+dbsrrohit@users.noreply.github.com>
2025-04-05 22:28:04 +0000

    conversion done

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,15 @@
+let score = "33"
+
+console.log(score);
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0;
+// 1=> true; 0 => flase;
+// "" => false;
+// "Rohit" => true;
