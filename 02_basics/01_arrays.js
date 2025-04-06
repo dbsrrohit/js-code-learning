fb2aa552409a6c60b72465192d1f3475c3da731b
Rohit Prabhakar <54357263+dbsrrohit@users.noreply.github.com>
2025-04-06 22:09:45 +0000

    arrat part 1 done

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,35 @@
+// Arrays
+// accessing array by reference
+const Names = ["rohit", "Prabhakar","Bobby"]
+
+console.log(Names[0]);
+
+const myArr = new Array(1,2,3,4)
+
+console.log(myArr[1]);
+
+// Array methods
+
+// myArr.push(6)
+// myArr.push(8)
+
+// console.log(myArr);
+
+// myArr.pop()
+
+// console.log(myArr);
+
+// myArr.unshift(0)
+
+// console.log(myArr);
+
+// myArr.shift()
+
+// console.log(myArr);
+
+// console.log(myArr.indexOf(3));
+
+
+// console.log(myArr.includes(9));
+
+// slice, splice
