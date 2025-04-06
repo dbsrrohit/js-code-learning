eeaf10462752ec900a9dc817a1c801b9c64f7cd0
Rohit Prabhakar <54357263+dbsrrohit@users.noreply.github.com>
2025-04-06 19:52:02 +0000

    Date done

---
+++ 01_basics/07_date.js
@@ -0,0 +1,8 @@
+// Begining of Jan, 01 1970 onwards
+
+console.log(Date());
+console.log(Date.now());
+const newdate = new Date();
+console.log(`newDate is ${newdate}`);
+const date1 = newdate.getDate();
+console.log(date1);
