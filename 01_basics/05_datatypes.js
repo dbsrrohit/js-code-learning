bd37c1b7a429284480c0dbc1740ffbb95beb1e64
Rohit Prabhakar <54357263+dbsrrohit@users.noreply.github.com>
2025-04-06 17:21:10 +0000

    all done

---
+++ 01_basics/05_datatypes.js
@@ -0,0 +1,13 @@
+// Primitive or Non-Primitive
+
+// Primitive - 
+// 7 type  - 
+// String, Number, Boolean, Null, Undefined, Symbol, BigInt
+// These are called by value and 
+// a copy of these are passed and only copy is changed
+
+
+// Refrence type or Non Primitive 
+
+// Arrays, Objects, Functions
+
